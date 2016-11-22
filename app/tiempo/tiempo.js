d108b53002bf7851d58d8b1249f8ab29acf46082
Joserra <joserra.o@gmail.com>
2016-11-22 22:27:28 +0100

    boton Buscar y conexión API

--- app/tiempo/tiempo.js
+++ app/tiempo/tiempo.js
@@ -1,15 +1,24 @@
 angular.module('eltiempo')
 
-    .controller('tiempo', function($scope){
+    .controller('tiempo', function($scope, $http){
         //Comienzo declaracion
         $scope.ciudad ="";
+        $scope.tiempo = "";
         //finalizo declaracion
 
         $scope.buscarCiudad = function () {
+            console.log('buscar ciudad');
             
-
-
+            //llamada a la api
+            var url = "http://api.openweathermap.org/data/2.5/weather?q="+$scope.ciudad+"&appid=1fea7bb734ba36400c5b8f22710e0d80";
+            $http.get(url) 
+                .success (function(result) {
+                    console.log(result);
+                    $scope.tiempo = result
+                })
+                .catch(function(error){
+                    console.log(error)
+                })
 
         }
-
-    })
+    });
